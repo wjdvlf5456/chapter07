ac1c79514fccb96cb9fad69cd4af9925ce3b0e29
최정필 <wjdvlf1407@gmail.com>
2023-08-30 19:51:42 +0900

    111

--- src/echo/Client.java
+++ src/echo/Client.java
@@ -9,63 +9,90 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.InetSocketAddress;
 import java.net.Socket;
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class Client {
+
 	public static void main(String[] args) throws IOException {
-
-		//
+		
 		Socket socket = new Socket();
-
+		
 		System.out.println("<클라이언트 시작>");
-		System.out.println("==============================");
-
-		System.out.println("[서버에 연결을 시도합니다.]");
-		socket.connect(new InetSocketAddress("127.0.0.1", 10001));
-
-		System.out.println("[서버에 연결되었습니다.]");
-
-		// 메세지보내기용 스트림
-		// OutputStream os = new FileOutputStream("phonebook.txt"); 이거 대신 ↓
-		OutputStream os = socket.getOutputStream();
+		System.out.println("============================");
+		
+		System.out.println("[서버에 연결을 요청합니다]");
+		socket.connect(new InetSocketAddress("192.168.188.23", 10001));
+		
+		System.out.println("[서버에 연결 되었습니다.]");
+		
+		// 메세지 보내기용 스트림
+		OutputStream os = socket.getOutputStream();	// new 불필요, 알아서 세팅되어있음
 		OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8");
 		BufferedWriter bw = new BufferedWriter(osw);
-
+		
 		// 메세지 받기용 스트림
 		InputStream is = socket.getInputStream();
 		InputStreamReader isr = new InputStreamReader(is, "UTF-8");
 		BufferedReader br = new BufferedReader(isr);
-
+		
+		
 		// 스캐너
-		Scanner sc = new Scanner(System.in);
-
-		while (true) {
-			// 메세지 입력
-			String str = sc.nextLine();
-
-			if (str.equals("/q")) {
+		// Scanner sc = new Scanner(System.in);
+		
+		// 스캐너 대용 (키보드 연결 / 학습용)
+		InputStream in = System.in;
+		InputStreamReader sisr = new InputStreamReader(in, "UTF-8");
+		BufferedReader sbr = new BufferedReader(sisr);
+		
+		// 반복문 적용
+		while(true) {
+			// 키보드 입력값
+			// String str = sc.nextLine();
+			
+			String str = sbr.readLine();
+			
+			if(str.equals("/q")) {
 				break;
+			}else if(str.equals("-help")) {
+				System.out.println("how can i help you");
+				
 			}
-
+			
+			
+			
+			
 			// 메세지 보내기
 			bw.write(str);
+			// 현재 정보량이 매우 적어 전송이 되지 않음(손님을 더 기다리는 중)
 			bw.newLine();
-			bw.flush();
-
+			bw.flush();	// 강제 송출
+			
 			// 메세지 받기
 			String reMsg = br.readLine();
-			System.out.println("server : [ " + reMsg + " ]");
-
+			System.out.println("server: [" + reMsg + "]");
+			
 		}
-
-		System.out.println("==============================");
-		System.out.println("<클라이언트 종료>");
-
-		sc.close();
+		
+		System.out.println("=============================");
+		//System.out.println("<클라이언트 종료>");
+		
+		// println 대용(학습용)
+		OutputStream out = System.out;
+		OutputStreamWriter posw = new OutputStreamWriter(out);
+		BufferedWriter pbw = new BufferedWriter(posw);
+		
+		pbw.write("<클라이언트 종료>");
+		pbw.newLine();
+		pbw.flush();
+		
+		//sc.close();
+		sbr.close();
 		br.close();
 		bw.close();
 		socket.close();
-
+		
+		
+		
 	}
 
 }
--- src/echo/Server.java
+++ src/echo/Server.java
@@ -1,48 +1,66 @@
 package echo;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
+
 	public static void main(String[] args) throws IOException {
-
-		ServerSocket ServerSocket = new ServerSocket();
-
-		// ServerSocket.bind(ip,port);
-		// ServerSocket.bind(new InetSocketAddress(ip, port));
-		// ip 검색 --> 명령 프롬프트 ipconfig --> IPv4 주소 체크 / mas : ifconfig
-		// ip는 .이 있어서 문자열이라서 " " 안에 작성
-		// en1 : 192.168.35.88
-		// "127.0.0.1", 5000
-		ServerSocket.bind(new InetSocketAddress("127.0.0.1", 10001)); // port번호는 개인이 설정
-
-		// accept() : 허락할꺼야
-		System.out.println("<서버시작>");
-		System.out.println("==============================");
+		// TCP 소켓 프로그래밍 - 서버 소켓
+		ServerSocket serverSocket = new ServerSocket();
+		serverSocket.bind(new InetSocketAddress("192.168.188.23", 10001)); // (ip주소, port)
+		
+		System.out.println("<서버 시작>");
+		System.out.println("============================================================");
 		System.out.println("[연결을 기다리고 있습니다.]");
-
-		// 반복 시작////////////
-		while (true) {
-			// 쓰레드 사용
-			Socket socket = ServerSocket.accept();
-
-			// 쓰레드 - 출장보내기(출장나가서 스트림봅강하고 대화해라)
-			Thread thread = new ServerThread(socket);
-			thread.start();
+		
+		Socket socket = serverSocket.accept();	// 소켓 객체 생성 command
+		System.out.println("[클라이언트가 연결되었습니다]");
+		
+		// 메세지 받기용 스트림
+		InputStream is = socket.getInputStream();
+		InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+		BufferedReader br = new BufferedReader(isr);
+		
+		// 메세지 보내기용 스트림
+		OutputStream os = socket.getOutputStream();
+		OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8");
+		BufferedWriter bw = new BufferedWriter(osw);
+		
+		// 클라이언트 while에 대응하여 반복 수령 필요함
+		while(true) {
+			// 메세지 받기
+			String msg = br.readLine();
+			
+			
+			
+			if(msg==null) {
+				break;
+			}
+			
+			System.out.println("최정필:" + msg);
+			// 메세지 보내기
+			bw.write(msg);
+			bw.newLine();
+			bw.flush();
 		}
-
-		// 쓰레드
-
-		// 반복 종료 ///////////////////////////////////
-		/*
-		 * System.out.println("==============================");
-		 * System.out.println("<서버종료>");
-		 * 
-		 * br.close(); bw.close(); socket.close(); ServerSocket.close();
-		 */
-
+		
+		
+		System.out.println("============================================================");
+		System.out.println("<서버 종료>");
+		bw.close();
+		br.close();
+		socket.close();
+		serverSocket.close();
+		
 	}
 
 }
